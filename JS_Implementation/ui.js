0b282396b7e56896e5f58501cecdf0ec34021905
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-06-16 11:26:56 +0300

    Update and rename ui.js to JS_Implementation/ui.js

--- JS_Implementation/ui.js
+++ JS_Implementation/ui.js
@@ -6,22 +6,28 @@ const infoDiv = document.getElementById("info");
 const drawBtn = document.getElementById("draw-btn");
 
 function updateUI() {
-  // Update board
-  boardDiv.innerHTML = "";
-  const tiles = game.currentBoard();
-  tiles.forEach(tile => {
-    const el = document.createElement("div");
-    el.className = "tile";
-    el.textContent = `[${tile[0]}|${tile[1]}]`;
-    boardDiv.appendChild(el);
-  });
+    boardDiv.innerHTML = "";
+    const tiles = game.currentBoard();
+    tiles.forEach(tile => {
+      const el = document.createElement("img");
+      const [a, b] = tile[0] <= tile[1] ? tile : [tile[1], tile[0]];
+      el.src = `images/dominoes/domino-${a}-${b}.png`;
+      el.alt = `[${tile[0]}|${tile[1]}]`;
+      el.className = "domino-img board-tile";
+      boardDiv.appendChild(el);
+    });
 
   // Update hand
   handDiv.innerHTML = "";
   const hand = game.playerHand(game.currentPlayer);
   hand.forEach(tile => {
     const btn = document.createElement("button");
-    btn.textContent = `[${tile[0]}|${tile[1]}]`;
+    const img = document.createElement("img");
+    const [a, b] = tile[0] <= tile[1] ? tile : [tile[1], tile[0]];
+    img.src = `images/dominoes/domino-${a}-${b}.png`;
+    img.alt = `[${a}|${b}]`;
+    img.className = "domino-img";
+    btn.appendChild(img);
     btn.className = "tile";
     if (game.validMoves(tile)) {
       btn.classList.add("green");
